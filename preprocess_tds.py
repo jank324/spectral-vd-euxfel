a588ad5a4a2df17ede0a751c15283282966f2c45
Jan Kaiser <jan.kaiser@desy.de>
2021-04-15 13:19:25 +0200

    Added longitudinal callibration

--- preprocess_tds.py
+++ preprocess_tds.py
@@ -11,19 +11,12 @@ import pandas as pd
 from mint.snapshot import SnapshotDB
 
 
-def load(path):
-    """Load data in Sergey's `SnapshotDB` format."""
-    snapshot_db = SnapshotDB(path)
-    df = snapshot_db.load()
-    return df
-
-
 def reduce_to_relevant_channels(df):
     """Reduce originally recorded data to a simple and readable DataFrame of only what is needed to get current profiles from TDS."""
 
     is_beam_on = df["XFEL.UTIL/BUNCH_PATTERN/CONTROL/BEAM_ALLOWED"].astype("bool")
     l1_chirp_phase = df["XFEL.RF/LLRF.SUMVOLTAGE_CTRL/L1/SUMVOLTAGE.CHIRP.SP.1"]
-    charge = df["XFEL.DIAG/CHARGE.ML/TORA.25.I1/CHARGE.ALL"]
+    charge = df["XFEL.DIAG/CHARGE.ML/TORA.25.I1/CHARGE.ALL"] * 10e-9
     tds_image = df["XFEL.DIAG/CAMERA/OTRA.473.B2D/IMAGE_EXT_ZMQ"].apply(lambda path: pickle.load(open("original/"+path[2:-4]+".pcl", "rb")))
 
     reduced = pd.DataFrame({"is_beam_on": is_beam_on,
@@ -40,25 +33,29 @@ def denoise(image):
     return denoised
 
 
-def extract_current_profile(row, seconds_per_pixel):
+def extract_current_profile(row, shear, pixel_size):
     """Extract current profile."""
 
+    seconds_per_pixel = pixel_size / shear
+
     view = row["tds_image"].sum(axis=0)
-    current = row["charge"] * 10e-9 / seconds_per_pixel * view / view.sum()
+    current = row["charge"] / seconds_per_pixel * view / view.sum()
+    times = np.arange(len(current)) * seconds_per_pixel
 
     xs = np.arange(len(current))
     model = GaussianModel()
     guess = model.guess(current, x=xs)
     fit = model.fit(current, guess, x=xs)
 
-    low = int(fit.params["center"].value - 4 * fit.params["sigma"].value)
-    high = int(fit.params["center"].value + 4 * fit.params["sigma"].value)
-    extracted = current[low:high]
+    a = int(fit.params["center"].value - 5 * fit.params["sigma"].value)
+    b = int(fit.params["center"].value + 5 * fit.params["sigma"].value)
+    extracted = current[a:b]
+    extracted_times = times[a:b] - fit.params["center"].value * seconds_per_pixel
 
-    return extracted
+    return np.vstack((extracted_times, extracted))
 
 
-def preprocess_tds(tds, seconds_per_pixel, plot=False):
+def preprocess_tds(tds, shear, pixel_size, plot=False):
     tds_reduced = reduce_to_relevant_channels(tds)
 
     background = tds_reduced.loc[1:6,"tds_image"].mean()
@@ -75,7 +72,7 @@ def preprocess_tds(tds, seconds_per_pixel, plot=False):
 
     preprocessed["tds_image"] = preprocessed["tds_image"].apply(denoise)
 
-    extract = partial(extract_current_profile, seconds_per_pixel=seconds_per_pixel)
+    extract = partial(extract_current_profile, shear=shear, pixel_size=pixel_size)
     preprocessed = preprocessed.apply(extract, axis=1)
 
     if plot:
@@ -89,19 +86,24 @@ def plot_profiles(preprocessed):
     for i, chirp in enumerate(preprocessed.index.values):
         plt.subplot(3, 6, i+1)
         plt.title(f"Chirp = {chirp}")
-        plt.plot(preprocessed[chirp])
+        plt.plot(preprocessed[chirp][0], preprocessed[chirp][1])
+        plt.xlabel("Time (s)")
+        plt.ylabel("Current (A)")
     plt.tight_layout()
     plt.show()
 
 
-def save_preprocessed_tds(input_path, output_path, seconds_per_pixel, plot=False):
-    tds = load(input_path)
-    preprocessed = preprocess_tds(tds, seconds_per_pixel, plot=plot)
+def save_preprocessed_tds(input_path, output_path, shear, pixel_size, plot=False):
+    snapshot_db = SnapshotDB(input_path)
+    tds = snapshot_db.load()
+
+    preprocessed = preprocess_tds(tds, shear, pixel_size, plot=plot)
+    
     with open(output_path, "wb") as file:
         pickle.dump(preprocessed, file)
 
 # Example call
-# python3 preprocess_tds.py original/20210221-01_30_17_scan_phase1.pcl tds_feb_phase1.pkl --seconds_per_pixel 4.38912e-13 --plot
+# python3 preprocess_tds.py original/20210221-01_30_17_scan_phase1.pcl tds_feb_phase1.pkl --shear 0.0027162e-12 --pixel_size 5.435999948531389e-6 --plot
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Preprocess recorded TDS data to current profiles")
@@ -109,10 +111,16 @@ if __name__ == "__main__":
                         help="Path to the input file")
     parser.add_argument("output_path", type=str,
                         help="Path to write the preprocessed data to")
-    parser.add_argument("--seconds_per_pixel", type=float, required=True,
-                        help="Calibration for seconds per pixel in the TDS images")
+    parser.add_argument("--shear", type=float, required=True,
+                        help="Longitudinal calibration in m/s")
+    parser.add_argument("--pixel_size", type=float, required=True,
+                        help="Width of the pixels of the camera used in m")
     parser.add_argument("--plot", default=False, action="store_true",
                         help="Show a plot of the extracted current profiles")
     args = parser.parse_args()
 
-    save_preprocessed_tds(args.input_path, args.output_path, args.seconds_per_pixel, plot=args.plot)
+    save_preprocessed_tds(args.input_path,
+                          args.output_path,
+                          args.shear,
+                          args.pixel_size,
+                          plot=args.plot)
