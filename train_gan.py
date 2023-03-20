a2e6267d750a293b914f419b22b78e76daa6468a
Jan Kaiser <jan.kaiser.email@googlemail.com>
2023-03-20 17:10:59 +0100

    Outline GAN architecture

--- train_gan.py
+++ train_gan.py
@@ -1,2 +1,33 @@
 # Generator outputs bunch length and shape
-# Discriminator is given bunch length and shape of real and generated example and has to learn to tell them apart as real or generated
+# Discriminator is given bunch length and shape of real and generated example and has to
+# learn to tell them apart as real or generated
+
+from torch import nn
+
+
+class Generator(nn.Module):
+    """
+    Takes as input a formfactor and the range over which we want the longitudinal
+    current profile to be inferred. Outputs a longitudinal current profile.
+
+    Convolve 1-dimensionally the formfactor, maybe do some MLP on the range. Flatten the
+    formfactor and concatenate with the output comming from the range. Maybe do some
+    more MLP and then deconvolve 1-dimensionally to get the longitudinal current
+    profile.
+    """
+
+    pass
+
+
+class Critic(nn.Module):
+    """
+    Take as input a longitudinal current profile and the range over which that current
+    profile is given. Output value between 1 and 0 on whether the input example was a
+    real measured one or a prediction from the Generator.
+
+    Convolve the current profile 1-dimensionally, then flatten. Maybe do some MLP on the
+    range. Concateneate decoded current profile and range, then do more MLP down to the
+    1-element output.
+    """
+
+    pass
